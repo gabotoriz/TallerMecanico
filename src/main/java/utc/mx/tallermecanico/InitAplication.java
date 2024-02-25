a202faa1bd76f08702b416e4afe28a64b58584f0
GabTrax <g.garcia.t839@edu.utc.mc>
2024-02-24 18:57:00 -0600

    Se agrego el cambio de vista con base en la hoja de rutas (View.java)

--- src/main/java/utc/mx/tallermecanico/InitAplication.java
+++ src/main/java/utc/mx/tallermecanico/InitAplication.java
@@ -1,21 +1,26 @@
 package utc.mx.tallermecanico;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 import java.io.IOException;
 
+import static utc.mx.tallermecanico.ViewSwitcher.setScene;
+import static utc.mx.tallermecanico.ViewSwitcher.switchTo;
+
 public class InitAplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(InitAplication.class.getResource("selection-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        var scene = new Scene(new Panel());
+        setScene(scene);
+        switchTo(View.Selection);
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
